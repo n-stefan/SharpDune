200e8817413834d25633dd89edf23d0bd6442bce
Stefan Negulescu <tarthedark@gmail.com>
2022-09-02 20:24:58 +0300

    Simplify code

--- Audio/DspNone.cs
+++ Audio/DspNone.cs
@@ -4,25 +4,13 @@ namespace SharpDune.Audio;
 
 class DspNone
 {
-    internal static bool DSP_Init()
-    {
-        return true;
-    }
+    internal static bool DSP_Init() => true;
 
-    internal static void DSP_Uninit()
-    {
-    }
+    internal static void DSP_Uninit() {}
 
-    internal static byte DSP_GetStatus()
-    {
-        return 0;
-    }
+    internal static byte DSP_GetStatus() => 0;
 
-    internal static void DSP_Play(byte[] data)
-    {
-    }
+    internal static void DSP_Play(byte[] data) {}
 
-    internal static void DSP_Stop()
-    {
-    }
+    internal static void DSP_Stop() {}
 }
--- Audio/MidiNone.cs
+++ Audio/MidiNone.cs
@@ -4,25 +4,13 @@ namespace SharpDune.Audio;
 
 class MidiNone
 {
-    internal static bool Midi_Init()
-    {
-        return true;
-    }
+    internal static bool Midi_Init() => true;
 
-    internal static void Midi_Uninit()
-    {
-    }
+    internal static void Midi_Uninit() {}
 
-    internal static void Midi_Reset()
-    {
-    }
+    internal static void Midi_Reset() {}
 
-    internal static void Midi_Send(uint data)
-    {
-    }
+    internal static void Midi_Send(uint data) {}
 
-    internal static ushort Midi_Send_String(byte[] data, ushort len)
-    {
-        return len;
-    }
+    internal static ushort Midi_Send_String(byte[] data, ushort len) => len;
 }
