db63cd51d6f96fda9d5bd517f0a4225fd1f621aa
Stefan Negulescu <tarthedark@gmail.com>
2025-02-12 20:37:40 +0200

    Fix conditional compilation

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -9,6 +9,7 @@ global using System.Collections;
 global using System.Diagnostics;
 global using System.Globalization;
 global using System.Reflection;
+global using System.Runtime.CompilerServices;
 global using System.Runtime.InteropServices;
 global using System.Text;
 global using System.Text.RegularExpressions;
